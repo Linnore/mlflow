bc232e4d53bc8fdd926ed5ba55dc14e297e0ad0f
linnore.li <linnore.li@fcc-analytics.com>
2026-02-25 16:08:51 +0800

    Manually drop the check constraint before alter the status column

--- mlflow/store/db_migrations/versions/cfd24bdc0731_update_run_status_constraint_with_killed.py
+++ mlflow/store/db_migrations/versions/cfd24bdc0731_update_run_status_constraint_with_killed.py
@@ -48,17 +48,22 @@ def upgrade():
     table_args = (
         [] if Version(alembic.__version__) >= Version("1.7.0") else check_constraint_table_args
     )
+
     with op.batch_alter_table("runs", table_args=table_args) as batch_op:
         # Transform the "status" column to an `Enum` and define a new check constraint. Specify
         # `native_enum=False` to create a check constraint rather than a
         # database-backend-dependent enum (see https://docs.sqlalchemy.org/en/13/core/
         # type_basics.html#sqlalchemy.types.Enum.params.native_enum)
+
+        batch_op.drop_constraint("status", type_="check")
+
         batch_op.alter_column(
             "status",
             type_=Enum(
                 *new_run_statuses,
                 create_constraint=True,
                 native_enum=False,
+                name="status_type",
             ),
             existing_type=Enum(
                 *old_run_statuses,
